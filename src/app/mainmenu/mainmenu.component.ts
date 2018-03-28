0d21ab318c29f5c2a4ab2be43839135f0225e3ee
Guillaume Bellamy <guillaume.bellamy04@gmail.com>
2018-03-28 19:25:05 +0200

    created main menu

--- src/app/mainmenu/mainmenu.component.ts
+++ src/app/mainmenu/mainmenu.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'app-mainmenu',
@@ -10,6 +11,9 @@ export class MainmenuComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    $("#credits").click(function () {
+      $("#panel").toggle("slide");
+    });
   }
 
 }
